80b1928eac8a5678a3ca83ccf41c8700291e317c
GingerOchai <samuelgingermichaelochai@gmail.com>
2025-10-06 11:59:24 +0100

    final

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,13 @@
-import { Button} from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
+"use client"
+import {api} from "@workspace/backend/_generated/api"
+import {useQuery} from "convex/react"
 export default function Page() {
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello app/widgets</h1>
-        <Button size="sm">Button</Button>
-        <Input placeholder="This is " />
+    {JSON.stringify(users)}
       </div>
     </div>
   )
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,12 +1,13 @@
-import {Button} from '@workspace/ui/components/button'
+import {api} from "@workspace/backend/_generated/api"
+import {useQuery} from "convex/react"
 import {add} from '@workspace/math/add'
 export default function Page() {
+  const users = useQuery(api.users.getMany)
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello app/widgets</h1>
-        <Button size="sm">Button</Button>
-        {add(2, 2)}
+       {JSON.stringify(users)}
       </div>
     </div>
   )
