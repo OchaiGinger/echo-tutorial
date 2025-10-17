7a6f75a729da03febc1ff27e5dc28ebe1f9d8bec
GingerOchai <samuelgingermichaelochai@gmail.com>
2025-10-17 16:05:57 +0100

    03-Authentication

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,20 +1,33 @@
 "use client"
-import {api} from "@workspace/backend/_generated/api"
+
+import { api } from "@workspace/backend/_generated/api"
 import { Button } from "@workspace/ui/components/button"
-import {useMutation, useQuery} from "convex/react"
+import { useMutation, useQuery, Authenticated, Unauthenticated } from "convex/react"
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Page() {
   const users = useQuery(api.users.getMany)
   const addUser = useMutation(api.users.add)
   const delUser = useMutation(api.users.del)
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello app/web</h1>
-        <Button onClick={() => addUser()}>Add User</Button>
-    {JSON.stringify(users)}
 
-    <Button onClick={() => delUser()} className="" variant="destructive">Delete Users</Button>
+  return (
+    <>
+    <Authenticated>
+      <div className="flex items-center justify-center min-h-svh">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-2xl font-bold">Hello app/web</h1>
+          <UserButton/>
+          <Button onClick={() => addUser()}>Add User</Button>
+          {JSON.stringify(users)}
+
+          <Button onClick={() => delUser()} className="" variant="destructive">Delete Users</Button>
+        </div>
       </div>
-    </div>
+    </Authenticated>
+    <Unauthenticated>
+      <p>Must be signed in!</p>
+      <SignInButton>Sign in!</SignInButton>
+    </Unauthenticated>
+    </>
   )
 }
--- apps/web/components/providers.tsx
+++ apps/web/components/providers.tsx
@@ -2,13 +2,21 @@
 
 import * as React from "react"
 
-import { ConvexProvider, ConvexReactClient } from "convex/react"
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "" )
+import { ConvexProvider, ConvexReactClient } from "convex/react"
+import { ConvexProviderWithClerk } from "convex/react-clerk"
+import { useAuth } from '@clerk/nextjs'
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
+}
+
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL || "")
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-  <ConvexProvider client={convex}>
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
       {children}
-</ConvexProvider>
+    </ConvexProviderWithClerk>
   )
 }
